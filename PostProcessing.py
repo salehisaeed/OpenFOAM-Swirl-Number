ab50a3eebfaaa99ff48a9b3e873c3fddb6f227c2
Saeed Salehi <saeed.salehi@chalmers.se>
2025-06-17 07:39:40 +0200

    Updated PostProcessing.py

--- PostProcessing.py
+++ PostProcessing.py
@@ -14,11 +14,9 @@ regions = sorted(set(
     for name in os.listdir(postproc_dir)
     if name.startswith("swirl_")
 ))
-
     
 print(f"🔎 Found regions: {regions}")
 
-# --- Process Each Region ---
 swirl_data = {}
 
 fig = plt.figure(figsize=(8, 5))
@@ -29,7 +27,6 @@ for region in regions:
     num_path = postproc_dir / f"swirl_numerator_{region}/0/surfaceFieldValue.dat"
     denom_path = postproc_dir / f"swirl_denominator_{region}/0/surfaceFieldValue.dat"
 
-    # --- Read Data and Calculate Swirl Number ---
     num_df = pd.read_csv(num_path, sep='\s+', comment="#", names=["time", "num"])
     denom_df = pd.read_csv(denom_path, sep='\s+', comment="#", names=["time", "denom"])
     merged_df = pd.merge(num_df, denom_df, on="time", how="inner")
@@ -37,12 +34,8 @@ for region in regions:
     merged_df["swirl"] = merged_df["num"] / (merged_df["denom"] * radii)
     swirl_data[region] = merged_df
 
-    # --- Plotting ---
     plt.plot(merged_df["time"], merged_df["swirl"], label=f"{region}")
 
-# --- Publication-quality Figure Settings ---
-# plt.style.use("seaborn-v0_8-whitegrid")
-# plt.axi
 plt.xlabel("Time (s)", fontsize=15)
 plt.ylabel("Swirl Number ($S$)", fontsize=15)
 plt.xticks(fontsize=13)
